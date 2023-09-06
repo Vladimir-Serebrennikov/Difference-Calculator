80d7e960a5aeca73e5cea008dfdf886dea5a1152
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-09-06 19:10:23 +0300

    touched picocli reference

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine.run(new App(), args);
+    }
+
+    @Override
+    public void run() {
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+        } else if (versionRequested) {
+            System.out.println("Version 1.0"); // Замените на вашу версию
+        } else {
+            // Добавьте логику вашей команды здесь
+            System.out.println("Command logic goes here.");
+        }
     }
 }
+
+
